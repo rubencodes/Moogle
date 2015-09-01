f927a14ceac0724bf74e21f92c35a684eed8cbb7
Ruben Martinez Jr <rmartin@bowdoin.edu>
2015-09-01 14:41:35 -0700

    Restructured Homepage design; Added Results section and GET request to populate

--- MovieSearchApp/client/js/MovieSearchApp.js
+++ MovieSearchApp/client/js/MovieSearchApp.js
@@ -6,11 +6,28 @@ Template.Home.onRendered(function() {
 
 Template.Home.helpers({
 	//Helpers needed on the home page
+	searchResult: function() {
+		return Session.get("result");
+	}
 });
 
 Template.Home.events({
 	//Events that happen on the home page
 	'click button#SearchButton': function () {
 		//Search OMDB API
+		$.get("http://www.omdbapi.com/", {
+			t: $("input#SearchField").val(),
+			plot: "full",
+			r: "json"
+		}, function(data) {
+			Session.set("result", data);
+		});
+	}
+});
+
+Template.Result.helpers({
+	//Turns Rating into Percent
+	imdbPercent: function(rating) {
+		return rating*10;
 	}
 });
