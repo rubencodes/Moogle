0130964a7e3fe7c95a1ceeb6326f4b8a5edefe25
Ruben Martinez Jr <rmartin@bowdoin.edu>
2015-09-01 15:06:35 -0700

    Improved design of search results

--- MovieSearchApp/client/js/MovieSearchApp.js
+++ MovieSearchApp/client/js/MovieSearchApp.js
@@ -14,14 +14,16 @@ Template.Home.helpers({
 Template.Home.events({
 	//Events that happen on the home page
 	'click button#SearchButton': function () {
-		//Search OMDB API
-		$.get("http://www.omdbapi.com/", {
-			t: $("input#SearchField").val(),
-			plot: "full",
-			r: "json"
-		}, function(data) {
-			Session.set("result", data);
-		});
+		//Search OMDB API if textbox not empty
+		if($("input#SearchField").val().trim() != "") {
+			runOMDbSearch($("input#SearchField").val());
+		}
+	},
+	'keyup input#SearchField': function() {
+		//Search OMDB API if enter pressed and textbox not empty
+		if (event.keyCode == 13 && $("input#SearchField").val().trim() != "") {
+			runOMDbSearch($("input#SearchField").val());
+		}
 	}
 });
 
@@ -29,5 +31,21 @@ Template.Result.helpers({
 	//Turns Rating into Percent
 	imdbPercent: function(rating) {
 		return rating*10;
+	},
+	validIMDbScore: function() {
+		return this.imdbRating != "N/A";
+	},
+	validMetascore: function() {
+		return this.Metascore != "N/A";
 	}
 });
+
+function runOMDbSearch(query) {
+	$.get("http://www.omdbapi.com/", {
+		t: query,
+		plot: "short",
+		r: "json"
+	}, function(data) {
+		Session.set("result", data);
+	});
+}
